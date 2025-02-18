ddf3fe9a480c14876a329e1e51cf75f144055eb1
Ken <andersonkennethw@outlook.com>
2025-02-18 17:53:24 -0500

    Update main.c

--- Equipotential-Visualizer/main.c
+++ Equipotential-Visualizer/main.c
@@ -9,7 +9,6 @@
 #include <stdlib.h>
 #include <math.h>
 
-
 // define the spacial boundries of the simulation
 #define Xmin -5.0
 #define Xmax 5.0
@@ -28,8 +27,10 @@
 // define coulombs constant
 #define coul (8.99e9)
 
-
+// Initialize 2 dimensional array and the "dx & dy" values.
 double V[Nx][Ny];
+double dx = (Xmax - Xmin) / (Nx - 1);
+double dy = (Ymax - Ymin) / (Ny - 1);
 
 /**
  *  @brief Function calculates the potential difference of any provided point
@@ -39,44 +40,79 @@ double V[Nx][Ny];
  *  @param Px is the given point x value of the place to compute the difference
  *  @param Py is the given point y value of the place to compute the difference
  *
- *  @return Func rerurns the value of the potential difference at the given point
+ *  @return Func returns the value of the potential difference at the given point
  *
  *  @note The function currently assumes there is only one charge present at (0,0) with magnitude e
  */
 double calcVolt(int Px, int Py) {
-    
-    double dx = (Xmax - Xmin) / (Nx - 1);
-    double dy = (Ymax - Ymin) / (Ny - 1);
     
     double xGridPoint = Xmin + Px * dx;
     double yGridPoint = Ymin + Py * dy;
     
     double distanceToCharge = sqrt(pow(fabs(xGridPoint - chargeX), 2) + pow(fabs(yGridPoint - chargeY), 2));
     
-    //printf("Grid Point (%d, %d) -> (%.2f, %.2f)\n", Px, Py, xGridPoint, yGridPoint);
+    //printf("Grid Point (%d, %d) -> (%.2f, %.2f)\n", Px, Py, xGridPoint, yGridPoint); <-- Save for printing in case of error
     
     return (coul * chargeMag)/distanceToCharge;
 }
 
+// The setupGrid function loops through each element in the 2d array and calculates the potential difference of the point
 void setupGrid(void) {
     for (int i = 0; i < Nx; i++) {
         for (int j = 0; j < Ny; j++) {
-            V[i][j] = calcVolt(i, j);
+            V[i][j] = calcVolt(i, j); // Use to calculate the difference
         }
     }
 }
 
-void renderEqLines(void) {
-    printf("Render Later boy");
+int findFurthestWall(int Cx, int Cy) { // find the wall furthest from the charge
+    int distances[4];
+    int toRightWall = (Nx - Cx);
+    int toLeftWall = Cx;
+    int toTopWall = Cy;
+    int toBottomWall = (Ny - Cy);
+    
+    distances[0] = toTopWall;
+    distances[1] = toBottomWall;
+    distances[2] = toRightWall;
+    distances[3] = toLeftWall;
+    
+    int maxIndex = 1;
+    for (int i = 2; i <= 4; i++) {
+        if (distances[i] > distances[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+    
+    return maxIndex;
 }
 
+/**
+ * @note The plan is to use an array thats sized based on the distance from the charge to the furthest wall.
+ * The function will then look through each point on the line from the charge to the furthest wall and take the value of every 25 points, storing it in the array.
+ * Using each value, the program will then render the points where the values fall within a specified "grace" range of one of the values in the array as a gray color,
+ * and the array index the value falls under will decide the shade of gray the values should be.
+ *
+ * Later, use a for loop to iterate this for each charge using the same array, that way we dont need to allocate a ton of memory, and we can just use the one array to calculate lines for every charge
+ */
 
-
+// render
+void renderEqLines(int Cx, int Cy) {
+    printf("Pass\n");
+    
+    int directionToFurthestWall = findFurthestWall(Cx, Cy); // Use 0 for up, 1 for down, 2 for right, and 3 for left
+    
+}
 
 int main(int argc, const char * argv[]) {
+    int Cx, Cy;
+    
+    Cx = 50;
+    Cy = 50;
+    
     setupGrid();
     
-    renderEqLines();
+    renderEqLines(Cx, Cy);
     
     return 0;
 }
