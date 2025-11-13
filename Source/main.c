bdb6d9af81cf6f0cbe7f4945f27bf86a2769da5b
Shrewd-Man <andersonkennethw@outlook.com>
2025-11-13 10:47:10 -0500

    remove dead code

--- Source/main.c
+++ Source/main.c
@@ -18,56 +18,6 @@ double V[NX][NY];
 double dx = (XMAX - XMIN) / (NX - 1);
 double dy = (YMAX - YMIN) / (NY - 1);
 
-
-/**
- *  @brief Function calculates the potential difference of any provided point
- *
- *  The function uses the universal formula and currently computes that based on the points distance to the charge x and y
- *
- *  @param charge structure data with the charge values
- *  @param chargeCount total amount of charges
- *  @param xGrid The x grid point of the calculation
- *  @param yGrid The y grid point of the calculation
- *
- *  @return Func returns the value of the potential difference at the given point
- */
-/*double calcVolt(charge_t charge[], int chargeCount, int xGrid, int yGrid) {
-    double xPhys = XMIN + xGrid * dx;
-    double yPhys = YMIN + yGrid * dy;
-    double potential = 0.0;
-    for (int i = 0; i < chargeCount; i++) {
-        double distanceToCharge = sqrt(pow(xPhys - charge[i].spaceX, 2) + pow(yPhys - charge[i].spaceY, 2));
-        if(charge[i].sign == 1){
-            potential += (COULOMBS_CONSTANT * charge[i].magnitude) / (distanceToCharge + 1e-9);
-        } else {
-            potential += (COULOMBS_CONSTANT * (0 - charge[i].magnitude)) / (distanceToCharge + 1e-9);
-        }
-        //potential += (COULOMBS_CONSTANT * charge[i].magnitude) / (distanceToCharge + 1e-9);
-    }
-    return potential;
-}*/
-
-/**
- *  @brief Function calculates the potential difference of every point in the matrix
- *
- *  The function calls the calcVolt for every point in the space
- *  @param charge the structure containing the data for the first charge
- *  @param chargeCount the total amounts of charges
- *
- *  @note This function is currently a temporary system that does not allow for live rendering
- */
-/*void setupGrid(charge_t charge[], int chargeCount) {
-    for (int i = 0; i < NX; i++) {
-        for (int j = 0; j < NY; j++) {
-            V[i][j] = calcVolt(charge, chargeCount, i, j); // Use to calculate the difference
-            if (i % 500 == 0 && j % 250 == 0) {  // Sample every 500x250 points
-                printf("V[%d][%d] = %.2e\n", i, j, V[i][j]);
-            }
-        }
-    }
-}*/
-
-
 int main(int argc, const char * argv[]) {
     
     // create array of 4 charges
